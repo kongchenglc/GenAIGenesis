fa94d957b6560488e522ac1be891740a8f1aad7f
William Chiu <williamrchiu18@gmail.com>
2025-03-22 15:26:49 -0400

    update backend websocket to have HTML

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -4,3 +4,5 @@ from utils.websocketUtil import router as websocket_router
 app = FastAPI()
 
 app.include_router(websocket_router, prefix="/ws")
+
+
--- BackEnd/utils/websocketUtil.py
+++ BackEnd/utils/websocketUtil.py
@@ -3,8 +3,8 @@ from PIL import Image
 from io import BytesIO
 import json
 
-router = APIRouter()
 
+router = APIRouter()
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -18,10 +18,10 @@ async def websocket_endpoint(websocket: WebSocket):
 
                     API_response = {
                         "summary": "Hello", 
-                        "elements": ""
+                        "elements": "World"
                     }
                     two_string_response = json.dumps(API_response)
-                    await websocket.send_text(two_string_response)
+                    await websocket.send_json(two_string_response)
                 
                 except Exception as e:
                     print("Error processing text:", e)
@@ -33,16 +33,32 @@ async def websocket_endpoint(websocket: WebSocket):
                     #send image to AI
                     API_response = {
                         "summary": "Hello",
-                        "elements": ""
+                        "elements": "World"
                     }
                      #this custom data type will be filled by the API AI call
                     two_string_response = json.dumps(API_response)
 
-                    await websocket.send_text(two_string_response)
+                    await websocket.send_json(two_string_response)
                 except Exception as e:
                     print("Error processing image: ", e)
                     await websocket.send_text("Error processing image")
-            
+            elif "URL" in data:
+                URL_message = data["URL"]
+                try:
 
+
+                    #API call for URL message to Unpacker
+
+                    API_response = {
+                        "summary" : "test",
+                        "options" : "test",
+                        "HTML_Element" : {"testing":"response1"}
+                    }
+
+                    JSON_response = json.dumps(API_response)
+                    await websocket.send_json(JSON_response)
+                except Exception as e:
+                    print("Error processing HTML: ", e)
+                    await websocket.send_text("Error processing HTML.")         
     except WebSocketDisconnect:
         print("Client disconnected")
